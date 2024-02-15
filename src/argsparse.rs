ec1f4fbf87dfe9c6aa248ee4d48beb50bbd3deed
datawater <86855332+datawater@users.noreply.github.com>
2024-02-16 00:17:20 +0400

    Impelemented the argsparse cli for the main function

--- src/argsparse.rs
+++ src/argsparse.rs
@@ -37,8 +37,8 @@ impl PartialEq for SubCommand {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Default)]
-struct ConvertTo {
-    pub input_files: Vec<String>,
+pub struct ConvertTo {
+    pub input_file: String,
     pub outout_file: Option<String>,
     pub compress_headers: bool,
 }
@@ -56,7 +56,7 @@ fn usage(subcommand: Option<String>) {
     } else {
         match subcommand.as_ref().unwrap().as_str() {
             "convert_to" | "ct" | "t" => {
-                println!("Usage:\n\tcaissa convert_to [-o output_file] [input files]");
+                println!("Usage:\n\tcaissa convert_to [-o output_file] [input file]");
             }
 
             _ => {
@@ -76,9 +76,11 @@ impl Cli {
         let mut args = args();
 
         let mut i = 0;
-
+        
         args.nth(0);
-        while i < args.len() {
+        let len = args.len();
+        
+        while i < len {
             let arg = args.nth(0).unwrap();
 
             if se1f.subcommand == SubCommand::None && !arg.starts_with("--") {
@@ -115,22 +117,18 @@ impl Cli {
                 exit(0);
             }
 
+
             if arg == "-o" || arg == "--output" {
                 if se1f.subcommand == SubCommand::ConvertTo {
-                    if se1f.state.convert_to.is_some() {
+                    if se1f.state.convert_to.as_ref().unwrap().outout_file.is_some() {
                         eprintln!(
                             "Output file provided more than once. Run `caissa --help` for help."
                         );
                         exit(1);
                     }
 
-                    se1f.state.convert_to.as_mut().unwrap().outout_file = match args.nth(i + 1) {
+                    se1f.state.convert_to.as_mut().unwrap().outout_file = match args.nth(0) {
                         Some(filename) => {
-                            if !Path::new(&filename).exists() {
-                                eprintln!("The provided output file path ({filename}) doesn't exist. Run `caissa --help` for help.");
-                                exit(1);
-                            }
-
                             Some(filename)
                         }
 
@@ -146,18 +144,21 @@ impl Cli {
                     exit(1);
                 }
 
-                i += 1;
+                i += 2;
                 continue;
             }
 
             if Path::new(&arg).exists() {
                 if se1f.subcommand == SubCommand::ConvertTo {
-                    se1f.state
-                        .convert_to
-                        .as_mut()
-                        .unwrap()
-                        .input_files
-                        .push(arg.clone());
+                    if se1f.state.convert_to.as_ref().unwrap().input_file.len() != 0 {
+                        eprintln!(
+                            "Multiple Input files supplied when only one is expected. Run `caissa --help` for help."
+                        );
+
+                        exit(1);
+                    }
+
+                    se1f.state.convert_to.as_mut().unwrap().input_file = arg.clone();
                 } else {
                     eprintln!(
                         "Input file supplied when it's not needed. Run `caissa --help` for help."
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
+use std::{error::Error, fs::File};
+use std::io::Write;
+
+use argsparse::SubCommand;
+use pgn::PositionTable;
+
 mod argsparse;
 mod pgn;
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<(), Box<dyn Error>> {
     let cli = argsparse::Cli::new();
 
-    println!("{cli:?}");
+    if cli.subcommand == SubCommand::ConvertTo {
+        let mut pt: PositionTable = Default::default();
+
+        pt.from_game(cli.state.convert_to.as_ref().unwrap().input_file.clone());
+    
+        let output_file_path = cli.state.convert_to.as_ref().unwrap().outout_file.clone();
+
+        let mut output_file = File::create(if output_file_path == None {
+            "output.txt".to_string() // TODO: Change this to use a proper file path.
+        } else {
+            output_file_path.unwrap()  
+        })?;
+
+        writeln!(output_file, "{:?}", pt)?;
+    }
 
     Ok(())
 }
